8684798dafdecd229a0f2c1d867ae52ef48c1f4d
Anoop Jacob Thomas <anoopjt@gmail.com>
2023-05-03 17:38:20 +0530

    Trying to fix the linting errors.

--- factorial.py
+++ factorial.py
@@ -6,23 +6,23 @@ import time
 final_list = []
 
 def factorial(var_n):
-   """docstring for factorial
-   """
-   time.sleep(.1)
-   factorial = 1
-   for i in range (1, var_n + 1):
-      factorial = factorial * i
-   return factorial
+    """docstring for factorial
+    """
+    time.sleep(.1)
+    fact = 1
+    for i in range (1, var_n + 1):
+       fact = fact * i
+   return fact
 
 def sum_factorial():
-   """docstring for sum_factorial
-   """
-   for i in range(50):
-      final_list.append(factorial(i))
-      result=sum(final_list)
-      print("Final SUM is {}".format(result))
+    """docstring for sum_factorial
+    """
+    for i in range(50):
+       final_list.append(factorial(i))
+       result=sum(final_list)
+       print("Final SUM is {}".format(result))
    return result
 
 if __name__ == "__main__":
-   sum_factorial()
-   # Final SUM = 620960027832821612639424806694551108812720525606160920420940314 
+    sum_factorial()
+   # Final SUM = 620960027832821612639424806694551108812720525606160920420940314
