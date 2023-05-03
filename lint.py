ca9c47fcb0e68292cafdb704adaf9da1c9b4abe7
Anoop Jacob Thomas <anoopjt@gmail.com>
2023-05-03 17:49:45 +0530

    Trying to fix the linting errors. Changed the subscript to a method global_note - trial 6

--- lint.py
+++ lint.py
@@ -9,7 +9,7 @@ import sys
 from pylint import lint
 THRESHOLD = 2
 run = lint.Run(["factorial.py"], do_exit=False)
-score = run.linter.stats["global_note"]
+score = run.linter.stats.global_note
 if score < THRESHOLD:
     print("Linter failed: Score < threshold value")
     sys.exit(1)
